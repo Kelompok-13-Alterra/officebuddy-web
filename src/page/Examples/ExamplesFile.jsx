768b06c585fe8f534fd688b39b34a1133dd5f410
I Gusti Ngurah Febri Ananda Krisna <febrianandak552@gmail.com>
2023-05-22 19:44:09 +0800

    initiate repository

---
+++ src/page/Examples/ExamplesFile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ExamplesFile() {
+  return (
+    <div className="Example">
+      <h1 className="Example__Title">Ini Title</h1>
+      <button className="Example__Button">Ini Button</button>
+    </div>
+  );
+}
+
+export default ExamplesFile;
