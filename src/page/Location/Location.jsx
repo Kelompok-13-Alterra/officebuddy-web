2dd0279421fc56d383ed2a8e4e1f2c769128f21a
I Gusti Ngurah Febri Ananda Krisna <febrianandak552@gmail.com>
2023-05-23 18:22:52 +0800

    feat[location]: slicing UI location dari figma

--- src/page/Location/Location.jsx
+++ src/page/Location/Location.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import "./Location.css";
+import locationImg from "../../assets/img/location-img.png";
 
 function Location() {
   return (
-    <>
-      <div className="text">Location</div>
-    </>
+    <div className="location">
+      <main className="flex flex-col gap-16 mt-48 items-center justify-center">
+        <div className="container__title">
+          <h1 className="text-center">Office on whole across Indonesia</h1>
+          <p className="text-center">
+            Cari ruang kerja terbaik dimana saja kamu berada dengan fasilitas
+            dan keamanan yang sesuai
+          </p>
+        </div>
+        <img className="locantion__image" src={locationImg} alt="img" />
+        <div className="container__content flex flex-row align-middle justify-evenly gap-40 mb-10">
+          <div className="card card__mini">
+            <h2 className="text-center">Over 1.000 Office</h2>
+            <p className="text-center">
+              Terdapat lebih dari 1.000 di seluruh kota di Indonesia yang
+              tersedia untuk anda sehingga anda selalu mendapat ruang kerja
+              untuk anda
+            </p>
+          </div>
+          <div className="card">
+            <h2 className="text-center">Over 30 Seats for Coworking</h2>
+            <p className="text-center">
+              Anda selalu akan memiliki bangku di Coworking Space yang ada untuk
+              anda bekerja dengan ruang yang memiliki vibe asik untuk bekerja
+              bersama rekan kerja.
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
 
