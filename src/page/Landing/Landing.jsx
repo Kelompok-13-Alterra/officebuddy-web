0fc425a86ce1becbecc15250632f518d8cb3021e
Muhammad Albas Tomi <albustomi679@gmail.com>
2023-05-24 19:52:37 +0700

    feat[Landing]:slicing landing UI

--- src/page/Landing/Landing.jsx
+++ src/page/Landing/Landing.jsx
@@ -4,6 +4,10 @@ import SecondHeroLanding from "../../components/Landing/SecondHeroLanding";
 import ThirdLandingPage from "../../components/Landing/ThirdLandingPage";
 import FourthLanding from "../../components/Landing/FourthLanding";
 import Navbar from "../../components/Navbar/Navbar";
+import FifthLanding from "../../components/Landing/FifthLanding";
+import SixthLanding from "../../components/Landing/SixthLanding";
+import Footer from "../../components/Footer/Footer";
+import SeventhLanding from "../../components/Landing/SeventhLanding";
 
 export const Landing = () => {
   return (
@@ -13,6 +17,10 @@ export const Landing = () => {
       <SecondHeroLanding />
       <ThirdLandingPage />
       <FourthLanding />
+      <FifthLanding />
+      <SixthLanding />
+      <SeventhLanding />
+      <Footer />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ export default {
       colors: {
         "bg-body": "#FDFBFF",
         "bg-button": "#0074E5",
+        "bg-second": "#AAC7FF",
+        "bg-secondRounded": "#D6E3FF",
         "bg-fourth": "#7BACFF",
       },
     },
