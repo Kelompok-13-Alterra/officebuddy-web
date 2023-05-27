45d66729e39b0488d1236ec41b2c0a0f5e551010
Titto Mahogany A <tittomahogany@gmail.com>
2023-05-27 13:20:16 +0700

    feat[kantor]: menambahkan icon svg dan komonen input

---
+++ src/assets/svg/index.js
@@ -0,0 +1,21 @@
+import ArrowLeftIcon from "./ArrowLeftIcon";
+import ArrowRightIcon from "./ArrowRightIcon";
+import FilterIcon from "./FilterIcon";
+import CloseIcon from "./CloseIcon";
+import PlusIcon from "./PlusIcon";
+import ProjectorIcon from "./ProjectorIcon";
+import SpeakerIcon from "./SpeakerIcon";
+import WaterIcon from "./WaterIcon";
+import WhiteboardIcon from "./WhiteboardIcon";
+
+export {
+  FilterIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  CloseIcon,
+  PlusIcon,
+  ProjectorIcon,
+  SpeakerIcon,
+  WaterIcon,
+  WhiteboardIcon,
+};
