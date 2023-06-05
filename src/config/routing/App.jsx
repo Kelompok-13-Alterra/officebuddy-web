8ed7675e8ff8b4775a38ba3cc59b9fa424541ab6
Muhammad Albas Tomi <albustomi679@gmail.com>
2023-06-05 09:27:49 +0700

    feat[sidebar-dashboardNavbar]:menambahkan sidebar dan Navbar untuk halaman dashboard

--- src/config/routing/App.jsx
+++ src/config/routing/App.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Landing } from "../../page/Landing/Landing";
+import Location from "../../page/Location/Location";
+import ContactUs from "../../page/ContactUs/ContactUs";
+import Login from "../../page/Login/Login";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
