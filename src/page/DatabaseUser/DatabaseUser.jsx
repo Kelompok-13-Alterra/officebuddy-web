6c2b32bae1cc1b3172ae81eeaad7d6b89182a702
Titto Mahogany A <tittomahogany@gmail.com>
2023-06-13 09:58:58 +0700

    refactor[database-user]: refactor database user styling

--- src/page/DatabaseUser/DatabaseUser.jsx
+++ src/page/DatabaseUser/DatabaseUser.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import UsersIcon from "../../assets/img/users-icon.png";
-import CalendarIcon from "../../assets/img/calendar-icon.png";
 import RatingIcon from "../../assets/img/star-icon.png";
 import ProfileImg from "../../assets/img/Kantor.png";
 import ProfileImg2 from "../../assets/img/Ellipse_imgman.png";
@@ -42,7 +41,7 @@ const renderCustomizedLabel = ({
 const DatabaseUser = () => {
   return (
     <div className="p-8 bg-[#FDFBFF]">
-      <div className="grid grid-cols-3 gap-4 mb-[34px]">
+      <div className="grid grid-cols-2 gap-4 mb-[34px]">
         <div className="p-6 bg-white rounded-lg shadow-sm">
           <div className="mb-4">
             <img
@@ -59,22 +58,7 @@ const DatabaseUser = () => {
           </p>
           <h1 className="font-face-ro text-[24px] leading-8">510 Pengguna</h1>
         </div>
-        <div className="p-6 bg-white rounded-lg shadow-sm">
-          <div className="mb-4">
-            <img
-              className="w-[50px] h-[50px]"
-              src={CalendarIcon}
-              alt="Office Icon"
-            />
-          </div>
-          <h3 className="mb-1 font-face-ro-med text-[#44474E] leading-6">
-            Total Booking
-          </h3>
-          <p className="mb-1 font-face-ro text-[12px] text-[#8E9099] leading-4">
-            Total Pengguna booking pada hari ini
-          </p>
-          <h1 className="font-face-ro text-[24px] leading-8">150 Orang</h1>
-        </div>
+
         <div className="p-6 bg-white rounded-lg shadow-sm">
           <div className="mb-4">
             <img
@@ -96,7 +80,7 @@ const DatabaseUser = () => {
         </div>
       </div>
 
-      <div className="flex gap-9">
+      <div className="flex flex-col lg:flex-row lg:gap-9">
         <div className="basis-3/5">
           <div className="p-6">
             <div className="flex gap-2 items-center justify-between mb-5">
@@ -104,13 +88,13 @@ const DatabaseUser = () => {
                 <h2 className="font-face-ro font-semibold mb-1">
                   Pebandingan Data Pengguna Aktif dan Tidak Aktif
                 </h2>
-                <p className="font-face-ro text-[#46474A]">
+                <p className="font-face-ro text-[#46474A] text-sm">
                   Lihat detail data pengguna
                 </p>
               </div>
               <Link
                 to="/database-user/total-user"
-                className="font-face-ro font-semibold text-[#0074E5]"
+                className="font-face-ro font-semibold text-[#0074E5] text-end text-sm"
               >
                 Lihat Semua
               </Link>
@@ -162,13 +146,13 @@ const DatabaseUser = () => {
                 <h2 className="font-face-ro font-semibold mb-1">
                   Penilaian Pembeli
                 </h2>
-                <p className="font-face-ro text-[#46474A]">
+                <p className="font-face-ro text-[#46474A] text-sm">
                   Lihat penilaian pembeli
                 </p>
               </div>
               <Link
                 to="/database-user/user-rating"
-                className="font-face-ro font-semibold text-[#0074E5]"
+                className="font-face-ro font-semibold text-[#0074E5] text-end text-sm"
               >
                 Lihat Semua
               </Link>
@@ -225,13 +209,13 @@ const DatabaseUser = () => {
           <div className="flex gap-2 items-center justify-between mb-6">
             <div>
               <h2 className="font-face-ro font-semibold mb-1">Data Pesanan</h2>
-              <p className="font-face-ro text-[#46474A]">
+              <p className="font-face-ro text-[#46474A] text-sm">
                 Lihat Detail Pesanan Pembeli
               </p>
             </div>
             <Link
               to="/database-user/total-booking"
-              className="font-face-ro font-semibold text-[#0074E5]"
+              className="font-face-ro font-semibold text-[#0074E5] text-end text-sm"
             >
               Lihat Semua
             </Link>
@@ -247,11 +231,13 @@ const DatabaseUser = () => {
                   <h3 className="font-bold text-base">Michael Abraham</h3>
                   <p className="font-face-ro text-sm text-[#475467]">Kemarin</p>
                 </div>
-                <h3 className="font-face-ro">
+                <h3 className="font-face-ro text-sm">
                   Memesan <span className="font-bold">Wellspace</span>
                 </h3>
-                <p className="mb-2 text-sm font-face-ro">Co-Working Space</p>
-                <p className="font-face-ro-med bg-[#44474E1F] rounded-full text-sm text-[#1a1b1ea3] px-2 py-1 inline-block">
+                <p className="mb-2 font-face-ro text-[#666666]  text-[13px]">
+                  Co-Working Space
+                </p>
+                <p className="font-face-ro-med bg-[#44474E1F] rounded-full text-[13px] text-[#1a1b1ea3] px-2 py-1 inline-block">
                   Selesai
                 </p>
               </div>
@@ -269,12 +255,14 @@ const DatabaseUser = () => {
                   <h3 className="font-bold text-base">Irene Store</h3>
                   <p className="font-face-ro text-sm text-[#475467]">Kemarin</p>
                 </div>
-                <h3 className="font-face-ro">
+                <h3 className="font-face-ro text-sm">
                   Memesan <span className="font-bold">Wellspace</span>
                 </h3>
-                <p className="mb-2 text-sm font-face-ro">Co-Working Space</p>
-                <p className="font-face-ro-med bg-[#44474E1F] rounded-full text-sm text-[#1a1b1ea3] px-2 py-1 inline-block">
-                  Selesai
+                <p className="mb-2 font-face-ro text-[#666666] text-[13px]">
+                  Co-Working Space
+                </p>
+                <p className="font-face-ro-med bg-[#CEE5FF] rounded-full text-[13px] text-[#001D33] px-2 py-1 inline-block">
+                  Menunggu Pembayaran
                 </p>
               </div>
             </div>
