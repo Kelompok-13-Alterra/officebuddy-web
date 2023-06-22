0a2aa57e30ebf53a71542d4419d1c0bf78689430
Titto Mahogany A <tittomahogany@gmail.com>
2023-06-22 08:10:54 +0700

    refactor[ui]: refactor landing and navbar ui

--- src/components/Landing/ThirdLandingPage.jsx
+++ src/components/Landing/ThirdLandingPage.jsx
@@ -9,18 +9,15 @@ import LogoCleaning from "../../assets/img/cleaning.png";
 
 const ThirdLandingPage = () => {
   return (
-    <div className="p-3 w-full relative  py-7">
-      <div className="flex justify-start md:justify-center w-[600px} flex-col mx-auto items-center">
-        <p className="md:text-4xl text-2xl font-face-ro-bold mb-3 text-start md:text-center">
-          Mengapa Perusahaan
-        </p>
-        <p className="md:text-4xl text-2xl font-face-ro-bold text-start md:text-center">
-          Memilih Kami ?
+    <div className="px-4 py-8 md:py-[86px] w-full relative ">
+      <div className="mb-5">
+        <p className="w-full md:text-4xl text-2xl font-face-ro-bold leading-8 text-start md:text-center">
+          Mengapa Perusahaan <br /> Memilih Kami ?
         </p>
       </div>
-      <div className="grid grid-rows-2  w-full md:mb-4  mt-12 md:gap-24 justify-center items-center">
-        <div className="grid grid-cols-2 md:grid-cols-4 w-full gap-4 mx-auto">
-          <div className="flex gap-y-6 flex-col justify-center items-center">
+      <div className="grid grid-rows-2 w-full md:mb-4 mt-12 gap-10 md:gap-24 justify-center items-center">
+        <div className="grid grid-cols-2 md:grid-cols-4 w-full gap-10 mx-auto">
+          <div className="flex gap-y-2 md:gap-y-6 flex-col justify-center items-center">
             <img
               src={LogoDesktop}
               className="md:w-14 w-10 h-7 md:h-9"
@@ -30,30 +27,30 @@ const ThirdLandingPage = () => {
               Ruangan yang private
             </p>
           </div>
-          <div className="flex gap-y-6 flex-col justify-center items-center">
+          <div className="flex gap-y-2 md:gap-y-6 flex-col justify-center items-center">
             <img
               src={LogoInternet}
-              className="md:w-14 w-10 h-7 md:h-9"
+              className="md:w-14 w-10 h-7 md:h-9 object-contain object-center"
               alt="desktop"
             />
             <p className="font-face-ro-bold text-center text-xs md:text-base">
               Internet yang cepat
             </p>
           </div>
-          <div className="flex gap-y-6 flex-col justify-center items-center">
+          <div className="flex gap-y-2 md:gap-y-6 flex-col justify-center items-center">
             <img
               src={LogoKursi}
-              className="md:w-14 w-10 h-7 md:h-9"
+              className="md:w-14 w-10 h-7 md:h-9 object-contain object-center"
               alt="desktop"
             />
             <p className="font-face-ro-bold text-xs md:text-base text-center">
               Ruangan yang strategis
             </p>
           </div>
-          <div className="flex gap-y-4 flex-col justify-center items-center">
+          <div className="flex gap-y-2 md:gap-y-6 flex-col justify-center items-center">
             <img
               src={LogoKalender}
-              className="md:w-14 w-9 h-7 md:h-9"
+              className="md:w-14 w-9 h-7 md:h-9 object-contain object-center"
               alt="desktop"
             />
             <p className="font-face-ro-bold text-xs md:text-base text-center">
@@ -61,31 +58,31 @@ const ThirdLandingPage = () => {
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 md:gap-10 justify-between items-center mx-auto">
-          <div className="flex md:gap-y-6 flex-col justify-center mx-10 items-center">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-10 justify-between items-center mx-auto">
+          <div className="flex gap-y-2 md:gap-y-6 md: flex-col justify-center items-center">
             <img
               src={LogoCash}
-              className="md:w-14 w-10 h-7 md:h-9"
+              className="md:w-14 w-10 h-7 md:h-9 object-contain object-center"
               alt="desktop"
             />
             <p className="font-face-ro-bold text-xs md:text-base text-center">
               Pembayaran yang mudah
             </p>
           </div>
-          <div className="flex md:gap-y-6 flex-col justify-center items-center">
+          <div className="flex gap-y-2 md:gap-y-6 flex-col justify-center items-center">
             <img
               src={LogoOffice}
-              className="md:w-14 w-10 h-7 md:h-9"
+              className="md:w-14 w-10 h-7 md:h-9 object-contain object-center"
               alt="desktop"
             />
             <p className="font-face-ro-bold text-center text-xs md:text-base">
               Ruangan yang aman
             </p>
           </div>
-          <div className="flex  my-8 gap-y-6 flex-col w-full ml-[50%] md:ml-0 justify-between items-center">
+          <div className="col-span-2 md:col-span-1 flex gap-y-2 md:gap-y-6 flex-col w-full md:ml-0 justify-between items-center">
             <img
               src={LogoCleaning}
-              className="md:w-12 w-9 h-8 md:h-9"
+              className="md:w-12 w-9 h-8 md:h-9 object-contain object-center"
               alt="desktop"
             />
             <p className="font-face-ro-bold text-center text-xs md:text-base">
